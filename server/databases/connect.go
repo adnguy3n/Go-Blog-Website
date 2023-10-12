1bddf420cad92ba6cffbdcfca752a3f532b45b57
adnguy3n <nguyeant@mail.gvsu.edu>
2023-10-12 13:07:20 -0400

    Added GORM and Fiber and made a basic main.go

---
+++ server/databases/connect.go
@@ -0,0 +1,25 @@
+package databases
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+/*
+ * Defining the database. Used across the entire app to communicate with the Database.
+ */
+var TestDB *gorm.DB
+
+/*
+ * Connects to the Database or creates it if it doesn't exist.
+ * Currently set to test.db which can be found in the database folder.
+ */
+func ConnectTestDatabase() {
+	database, err := gorm.Open(sqlite.Open("database/test.db"), &gorm.Config{})
+
+	if err != nil {
+		panic("Failed to connect to database!")
+	}
+
+	TestDB = database
+}
