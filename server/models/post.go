06cbd0e52f0bba3498c2f2082dd3606ca68f8a13
adnguy3n <nguyeant@mail.gvsu.edu>
2023-12-07 12:13:43 -0500

    Added CreatedAt column to automatically timestamp

--- server/models/post.go
+++ server/models/post.go
@@ -1,10 +1,13 @@
 package models
 
+import "time"
+
 type Post struct {
-	PostID      uint   `json:"post_id" gorm:"unique; primaryKey"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Image       string `json:"image"`
-	UserID      uint   `json:"userID"`
-	Users       Users  `json:"user" gorm:"foreignKey:UserID"`
+	PostID      uint      `json:"post_id" gorm:"unique; primaryKey"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Image       string    `json:"image"`
+	UserID      uint      `json:"userID"`
+	Users       Users     `json:"user" gorm:"foreignKey:UserID"`
+	CreatedAt   time.Time `json:"created_at"`
 }
