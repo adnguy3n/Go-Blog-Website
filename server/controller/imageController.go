dc3311ecc0572a0ca22a6c0d5933a57d7af99a6a
adnguy3n <nguyeant@mail.gvsu.edu>
2023-12-07 12:12:27 -0500

    Reverting removal of the random character strings  to the filename.

--- server/controller/imageController.go
+++ server/controller/imageController.go
@@ -2,9 +2,28 @@ package controller
 
 import (
 	"log"
+	"math/rand"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+/*
+ * Lowercase alphabet.
+ */
+var letters = []rune("abcdefghijklmnopqrsuvwxyz")
+
+/*
+ * Returns a string of n random letters.
+ */
+func randomLetters(n int) string {
+	res := make([]rune, n)
+
+	for i := range res {
+		res[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(res)
+}
 
 /*
  * Upload Image.
@@ -20,7 +39,7 @@ func Upload(c *fiber.Ctx) error {
 	fileName := ""
 
 	for _, file := range files {
-		fileName = file.Filename
+		fileName = randomLetters(5) + "-" + file.Filename
 
 		if err := c.SaveFile(file, "./uploads/"+fileName); err != nil {
 			log.Println(err)
