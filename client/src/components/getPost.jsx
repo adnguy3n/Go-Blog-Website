538bb81e0c1dd96239d78a1083fbb07554006e0d
adnguy3n <nguyeant@mail.gvsu.edu>
2023-12-09 14:14:24 -0500

    Title and image for getpost now displays.

--- client/src/components/getPost.jsx
+++ client/src/components/getPost.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const GetPost = () => {
+const GetPost = ({ logout }) => {
     const [blogPost, setBlogPost] = useState();
-    const { id } = useParams();
+    const { post_id } = useParams();
     const navigate = useNavigate();
 
+    // Gets the blog post if the user is logged in.
     useEffect(() => {
         const loggedIn = localStorage.getItem("loggedIn");
 
@@ -18,20 +20,39 @@ const GetPost = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    // Get a specific blog post.
     const getBlogPost = () => {
         axios
-            .get()
+            .get(`api/getpost/${post_id}`, {withCredentials: true,})
 
-            .then()
+            // Handle Success
+            .then(function(response) {
+                setBlogPost(response?.data?.data);
+            })
 
-            .catch();
+            // Handle Error
+            .catch(function (error) {
+                console.log(error);
+                navigate("/login");
+                logout();
+            });
     }
 
     return (
-        <div>
-            
+        <div className="relative">
+            <div className="max-w-3xl mb-10 rounded overflow-hidden flex flex-col mx-auto text-center">
+                <div className="max-w-3xl mx-auto text-xl sm:text-4xl font-semibold inline-block hover:text-indigo-600 transition duration-500 ease-in-out mb-2">
+                    <p>{blogPost?.title}</p>
+                </div>
+
+                <img className="w-full h-96 my-4" src={blogPost?.image} />
+            </div>
         </div>
     )
+}
+
+GetPost.propTypes = {
+    logout: PropTypes.func
 }
 
 export default GetPost;
