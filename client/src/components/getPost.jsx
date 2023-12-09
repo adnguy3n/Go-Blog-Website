59f97fa92995d3c5dc135cb741374c2d95c59b6a
adnguy3n <nguyeant@mail.gvsu.edu>
2023-12-09 14:33:41 -0500

    Set image size limits and added post description.

--- client/src/components/getPost.jsx
+++ client/src/components/getPost.jsx
@@ -40,12 +40,29 @@ const GetPost = ({ logout }) => {
 
     return (
         <div className="relative">
-            <div className="max-w-3xl mb-10 rounded overflow-hidden flex flex-col mx-auto text-center">
+            <div className="max-w-3xl mb-10 rounded overflow-hidden block mx-auto text-center">
                 <div className="max-w-3xl mx-auto text-xl sm:text-4xl font-semibold inline-block hover:text-indigo-600 transition duration-500 ease-in-out mb-2">
                     <p>{blogPost?.title}</p>
                 </div>
 
-                <img className="w-full h-96 my-4" src={blogPost?.image} />
+                <center>
+                    <img 
+                        className="h-96 w-100% my-4"
+                        src={blogPost?.image}
+                    />
+                </center>
+                <p className="text-gray-700 text-base leading-8 max-w-2xl mx-auto">
+                     Author: {blogPost?.user?.first_name} {blogPost?.user?.last_name}
+                </p>
+                <hr/>               
+            </div>
+
+            <div className="max-w-3xl mx-auto">
+                <div className="mt-3 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                    <p>
+                        {blogPost?.description}
+                    </p>
+                </div>
             </div>
         </div>
     )
