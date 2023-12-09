015fe75e3b193e781e8465b5be2b26351b3e4bb7
adnguy3n <nguyeant@mail.gvsu.edu>
2023-12-09 03:28:00 -0500

    Updated home page.

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,34 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    const [count, setCount] = useState(0)
 
-    return (
-        <>    
-          <div>
-            <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank" rel="noreferrer">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
+  return (
+    <>
+      <div className="relative h-screen w-full flex items-center justify-center text-center bg-cover bg-center">
+        <div className="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
+
+        <main className="px-4 sm:px-6 lg:px-8 z-10">
+          <div className="text-center">
+            <h2 className="text-4xl tracking-tight leading-10 font-medium sm:text-5xl text-white sm:leading-none md:text-6xl">
+              Welcome home!
+            </h2>
+
+            <div className="mt-5 sm:mt-8 sm:flex justify-center">
+              <div className="rounded-md shadow">
+                <Link
+                  to="/createpost"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo transition duration-150 ease-in-out md:py-4 md:px-10"
+                >
+                  Create Post
+                </Link>
+              </div>
+              <div className="mt-3 sm:mt-0 sm:ml-3">
+                <Link
+                  to="/yourblog"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-regular rounded-md text-indigo-700 bg-indigo-100 hover:text-indigo-600 hover:bg-indigo-50 focus:outline-none focus:shadow-outline-indigo focus:border-indigo-300 transition duration-150 ease-in-out md:py-4 md:px-10"
+                >
+                  View Posts
+                </Link>
+              </div>
+            </div>
           </div>
-          <h1>Vite + React</h1>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-        </>
-      )
+        </main>
+      </div>
+    </>
+  )
 }
 
 export default Home;
