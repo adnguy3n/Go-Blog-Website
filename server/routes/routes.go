539c12b3c3e4613c373e56ad1690e17fe165865d
adnguy3n <nguyeant@mail.gvsu.edu>
2023-12-09 17:17:13 -0500

    Fixed update using post instead of patch.

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -19,7 +19,7 @@ func Routes(app *fiber.App) {
 	app.Post("api/post", controller.CreatePost)
 	app.Get("api/getAllPosts", controller.GetAllPosts)
 	app.Get("api/getPost/:post_id", controller.GetPost)
-	app.Post("api/update/:post_id", controller.UpdatePost)
+	app.Patch("api/update/:post_id", controller.UpdatePost)
 	app.Get("api/userposts", controller.GetUserPosts)
 	app.Delete("api/delete/:post_id", controller.DeletePost)
 	app.Post("api/upload", controller.Upload)
